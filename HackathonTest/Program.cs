33470e2243b26b1b32e8a3b8b694f47e4e490cc4
Sachin Thakur <sachin.thakur@enercross.com>
2026-05-01 02:04:58 -0500

    changes

--- HackathonTest/Program.cs
+++ HackathonTest/Program.cs
@@ -1,6 +1,5 @@
-using HackathonTest.Data;
+﻿using HackathonTest.Data;
 using Microsoft.EntityFrameworkCore;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +8,20 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
+// ✅ SQLite connection
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=app.db"));
 
 var app = builder.Build();
+
+
+// 🔥 IMPORTANT: Auto create DB + tables on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
